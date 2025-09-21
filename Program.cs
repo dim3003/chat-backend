2a1284e657d230cb65399bd874f24ad269f91ba2
dimitri <andre.d3003@gmail.com>
2025-09-21 13:38:31 +0200

    fix CORS issues

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
 using SignalRChat.Hubs;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                      });
+});
+
 
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 app.MapHub<ChatHub>("/chatHub");
 app.Run();
